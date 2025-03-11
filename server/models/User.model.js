e7af3d78f843b80492dfdbdbb97352b97af24294
qnchoyy <qni191488@abv.bg>
2025-03-11 20:25:57 +0200

    Create User.model.js

---
+++ server/models/User.model.js
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'First name is required'],
+        trim: true,
+    },
+    lastName: {
+        type: String,
+        required: [true, 'Last name is required']
+    },
+    email: {
+        type: String,
+        required: [true, 'Email is required'],
+        unique: true,
+        lowercase: true,
+        trim: true,
+        match: [/^\S+@\S+\.\S+$/, 'Please use a valid email']
+    },
+    password: {
+        type: String,
+        required: [true, 'Password is required'],
+        minlength: 6,
+        select: false
+    },
+    phone: {
+        type: String,
+        required: [true, 'Phone number is required']
+    },
+    role: {
+        type: String,
+        enum: ['customer', 'restaurant-owner', 'delivery-person', 'admin'],
+        default: 'customer'
+    },
+    active: {
+        type: Boolean,
+        default: true
+    }
+}, { timestamps: true });
+
+const User = mongoose.model('User', userSchema);
+
+export default User;

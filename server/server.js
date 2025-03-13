779aa69532bc81e3f674f4f2132d196e82158658
qnchoyy <qni191488@abv.bg>
2025-03-13 17:00:23 +0200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -2,10 +2,13 @@ import express from 'express'
 
 import { PORT } from './config/env.js';
 import connectToDatabase from './database/mongodb.js';
+import errorMiddleware from './middleware/error.middleware.js';
 
 const app = express();
 
 app.use(express.json());
+
+app.use(errorMiddleware)
 
 app.get('/', (req, res) => {
     res.send('Welcome the the Food delivery API!');

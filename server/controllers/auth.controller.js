91ece7949a10188f710f948711c0f25587dd6c85
qnchoyy <qni191488@abv.bg>
2025-03-15 22:39:08 +0200

    Add login controller

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,6 +1,9 @@
 import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 
 import User from "../models/User.model";
+
+import { JWT_SECRET, JWT_EXPIRES_IN } from '../config/env.js'
 
 
 export const register = async (req, res) => {
@@ -23,6 +26,29 @@ export const register = async (req, res) => {
         });
 
         res.status(201).json({ success: true, user });
+    } catch (error) {
+        res.status(500).json({ message: "Server error", error: error.message });
+    }
+}
+
+export const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        const user = await User.findOne({ email }).select("+password");
+
+        if (!user) {
+            return res.status(401).json({ message: "Invalid credentials" });
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (!isPasswordValid) {
+            return res.status(401).json({ message: "Invalid credentials" });
+        }
+
+        const token = jwt.sign({ id: user._id, role: user.role }, JWT_SECRET, { expiresIn: JWT_EXPIRES_IN });
+
+        res.status(200).json({ success: true, token, user: { id: user._id, email: user.email, role: user.role } });
     } catch (error) {
         res.status(500).json({ message: "Server error", error: error.message });
     }

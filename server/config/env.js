e83bf0a8b8512734bc617650c15bff2daccc8d1d
qnchoyy <qni191488@abv.bg>
2025-03-15 22:38:42 +0200

    Update env.js

--- server/config/env.js
+++ server/config/env.js
@@ -2,4 +2,4 @@ import { config } from "dotenv";
 
 config({ path: '.env' });
 
-export const { PORT, DB_URI } = process.env;
+export const { PORT, DB_URI, JWT_SECRET, JWT_EXPIRES_IN } = process.env;

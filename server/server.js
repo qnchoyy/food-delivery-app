c5884f8216823f33ef0308c0bd5f2d9f0cb9be32
qnchoyy <qni191488@abv.bg>
2025-03-30 22:35:28 +0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -4,12 +4,14 @@ import { PORT } from './config/env.js';
 import connectToDatabase from './database/mongodb.js';
 import errorMiddleware from './middleware/error.middleware.js';
 import authRouter from './routes/auth.routes.js';
+import restaurantRouter from './routes/restaurant.routes.js';
 
 const app = express();
 
 app.use(express.json());
 
 app.use('/api/auth', authRouter);
+app.use('/api/restaurants', restaurantRouter);
 
 app.use(errorMiddleware);
 
